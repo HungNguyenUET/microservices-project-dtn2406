5af0552c14982a3b7a0a6f9827db1045121aaedb
hungnguyenuet <hungnguyenuet@gmail.com>
2025-05-08 21:19:21 +0700

    api gateway add filter

--- api-gateway/src/main/java/vti/dtn/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/vti/dtn/api_gateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package vti.dtn.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
