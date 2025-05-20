4deecb66bddc239680a6d6e19146969db92dec28
hungnguyenuet <hungnguyenuet@gmail.com>
2025-05-20 21:32:19 +0700

    Oauth2

--- auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
+++ auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package vti.dtn.auth_service.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,27 +8,54 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import vti.dtn.auth_service.entity.Role;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import vti.dtn.auth_service.oauth2.repository.HttpCookieOAuthorizationRequestRepository;
+import vti.dtn.auth_service.oauth2.service.CustomOAuth2UserService;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {
+            "/oauth2/redirect",
+            "/oauth2/authorize",
+            "/oauth2/authorise",
+            "/oauth2/authorize/github",
+            "/oauth2/callback/github",
             "/api/v1/auth/login",
             "/api/v1/auth/register",
             "/api/v1/auth/refresh-token"
     };
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final HttpCookieOAuthorizationRequestRepository httpCookieOAuthorizationRequestRepository;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> {
-                   req.requestMatchers(WHITE_LIST_URL)
-                           .permitAll()
-                           .requestMatchers("/api/v1/accounts").hasAnyRole(Role.ADMIN.name())
-                           .anyRequest()
-                           .authenticated();
-                })
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL)
+                        .permitAll()
+                        .requestMatchers("/api/v1/accounts").hasAnyRole(Role.ADMIN.name())
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorise")
+                .authorizationRequestRepository(httpCookieOAuthorizationRequestRepository)
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*")
+                .and()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService)
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
 
         return http.build();
     }
